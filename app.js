b53fc640284c7e8e6e11850dfa3e325f834439ea
hogan <hogan.r.sherrow@vanderbilt.edu>
2022-06-04 22:05:14 -0500

    generated html via node

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
+const [myName, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-};
+const pageHTML = generatePage(myName, github);
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', pageHTML, err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
